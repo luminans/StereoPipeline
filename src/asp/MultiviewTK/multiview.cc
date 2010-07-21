9c73570cc0f5209d1daffae0f61e255a33f7af0c
Kyle Husmann <kyle.husmann@gmail.com>
2010-07-21 15:04:36 -0700

    Created barebones MultiviewTK module

---
+++ src/asp/MultiviewTK/multiview.cc
@@ -0,0 +1,29 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2010 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+
+#include <vw/FileIO.h>
+#include <vw/Image.h>
+#include <vw/Cartography.h>
+#include <vw/Math.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+using namespace vw;
+using namespace vw::cartography;
+
+int main( int argc, char *argv[] ) {
+  cout << "Hello world!" << endl;
+  return 0;
+}
